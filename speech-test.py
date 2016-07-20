b9e2443db2bf9d33c277806289fe348f17533a9c
DanikNik <nikolsky.dan@gmail.com>
2016-07-20 12:48:56 +0300

    test2(us)

--- speech-test.py
+++ speech-test.py
@@ -1,7 +1,7 @@
 import requests
 
 url = 'https://asr.yandex.net/asr_xml'
-params = {'uuid':'34353bf7-26ff-4ea8-85ee-a4164d3ab413', 'api_key' : 'f1233cf8-c27a-4bad-9b5e-04f6ed2f265a', 'topic' : 'notes', 'lang':'ru-RU'}
+params = {'uuid':'34353bf726ff4ea885eea4164d3ab413', 'key' : 'f1233cf8-c27a-4bad-9b5e-04f6ed2f265a', 'topic' : 'notes', 'lang':'ru-RU'}
 headers = {"Content-type": "audio/x-wav"}
 files = {'file': open('speech.wav', 'rb')}
 

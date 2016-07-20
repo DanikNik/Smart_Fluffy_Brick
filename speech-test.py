ec68686f76f40a4c6a5c4676cea6f7c6ecb2aeaa
DanikNik <nikolsky.dan@gmail.com>
2016-07-20 13:19:50 +0300

    Test3(success)
    This service occured to work badly with*.wav and to work ok with *.mp3

--- speech-test.py
+++ speech-test.py
@@ -2,8 +2,8 @@ import requests
 
 url = 'https://asr.yandex.net/asr_xml'
 params = {'uuid':'34353bf726ff4ea885eea4164d3ab413', 'key' : 'f1233cf8-c27a-4bad-9b5e-04f6ed2f265a', 'topic' : 'notes', 'lang':'ru-RU'}
-headers = {"Content-type": "audio/x-wav"}
-files = {'file': open('speech.wav', 'rb')}
+headers = {"Content-Type": "audio/x-mpeg-3"}
+files = {'file': open('speech.mp3', 'rb').read()}
 
 req = requests.post(url, params = params, headers = headers, files = files)
-print(req.status_code)
+print(req.text)

535e82f4dbb3fd5de22ed00c38549bb7e0791466
slava <push_sla@mail.ru>
2016-07-21 14:12:35 +0300

    21.07, It seems to be working better

--- src1/quest.py
+++ src1/quest.py
@@ -4,56 +4,58 @@ import requests, re
 
 #def NameBadRequest()#План "средниум"
 
-def BadRequest(ans):###Пока что так! -- план минимум
-	ans = "Пожалуйста, перефразируйте запрос"
+def GetThingQuest(request):
+	try:
+		for i in range(len(requests.post('https://ru.wikipedia.org/w/api.php', data={'action':'opensearch', 'search':request, 'format':'json'}).json())):
+			ans = (requests.post('https://ru.wikipedia.org/w/api.php', data={'action':'opensearch', 'search':request, 'format':'json'}).json())[2][i]
+			if len(ans) > 21:
+				qi = i
+				break
 
-def GetQuest(request):
-	for i in range(len(requests.post('https://ru.wikipedia.org/w/api.php', data={'action':'opensearch', 'search':request, 'format':'json'}).json())):
-		ans = (requests.post('https://ru.wikipedia.org/w/api.php', data={'action':'opensearch', 'search':request, 'format':'json'}).json())[2][i]
-		if len(ans) > 21:
-			qi = i
-			break
-	bb = 0
-	eb = 0
-	todel = ""
-	for i in ans:
-		#print(i, bb, eb, todel)
-		if i == "(":
-			bb += 1
-		elif i == ")":
-			eb += 1
-		if bb > 0:
-			todel += i
-		if (bb == eb) and (bb > 0):
-			ans = ans.replace(todel, "")
-			todel = ""
-			bb = 0
-			eb = 0
+		bb = 0
+		eb = 0
+		todel = ""
+		for i in ans:
+			#print(i, bb, eb, todel)
+			if i == "(":
+				bb += 1
+			elif i == ")":
+				eb += 1
+			if bb > 0:
+				todel += i
+			if (bb == eb) and (bb > 0):
+				ans = ans.replace(todel, "")
+				todel = ""
+				bb = 0
+				eb = 0
 
-	print(ans)
-	if len(ans) < 5:
-		ans = BadRequest(ans)
-	else:
+		print(ans)
 		if qi > 0:
 			ans = "Возможно, вы имели ввиду "+ans
-	return ans
+		return ans
+	except:
+		ans = "Я вас не понял, перефразируйте запрос"
+		return ans
 
-
-def ChtoTakoe(text, text_arr, url):
+def WhatIs(text, text_arr, url):
 	url = ""
-	mi = text_arr.index('такое') + 1
+	try:
+		mi = text_arr.index('такая') + 1
+	except:
+		try:
+			mi = text_arr.index('такой') + 1
+		except:
+			mi = text_arr.index('такое') + 1
 	mi1 = mi
 	while mi1 < len(text_arr):
 		if mi1 > mi:
 			url += " "
 		url += str(text_arr[mi1])
 		mi1 += 1
-	ans = GetQuest(url)
+	ans = GetThingQuest(url)
 	return ans
 
-#def KtoTakoi(): #План "средниум"
-#	global l_text, arr_l_text, url_text
-
+#def HowTo(text, text_arr, url):
 
 
 text = input()##Способ общения: input или open
@@ -61,12 +63,9 @@ l_text = text.lower()
 arr_l_text = l_text.split()
 url_text = ""
 
-if l_text.find('что такое') != -1:
-	ans = ChtoTakoe(l_text, arr_l_text, url_text)
-elif (l_text.find('кто такой') != -1) or (l_text.find('кто такая') != -1):
+if (l_text.find('кто такой') != -1) or (l_text.find('кто такая') != -1) or (l_text.find('кто такое') != -1) or (l_text.find('что такое') != -1):
 	#План "средниум -- "KtoTakoi(l_text, arr_l_text)
-	ans = ChtoTakoe()
-else:
-	SomeWhat()####!!!!!Не готово
-
+	ans = WhatIs(l_text, arr_l_text, url_text)
+elif (l_text.find('как приготовить')) or (l_text.find('как сварить')) or (l_text.find('как пожарить')) or (l_text.find('как испечь')) or (l_text.find('как запечь')) or (l_text.find('как сделать')):# or (l_text.find('как ')) or (l_text.find('')) or (l_text.find('')) or (l_text.find(''))
+	#ans = HowTo(l_text, arr_l_text, url_text)
 print("\n====================ANSWER==================\n"+ans)

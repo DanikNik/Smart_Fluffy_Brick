f42cce79fc75a5f9882e84e62352212a398dbbfd
slava <push_sla@mail.ru>
2016-07-21 13:17:46 +0300

    21.07, It seems to be working better

--- src1/quest.py
+++ src1/quest.py
@@ -1,48 +1,54 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 import requests, re
-from bs4 import BeautifulSoup
 
 #def NameBadRequest()#План "средниум"
 
-def BadRequest():###Пока что так! -- план минимум
+def BadRequest(ans):###Пока что так! -- план минимум
 	ans = "Пожалуйста, перефразируйте запрос"
 
-def GetQuest():
-	global url_text
-	#print(url_text)
-	#url = 'https://duckduckgo.com?q='+url_text+'&format=json&ia=about'
-	#print(url)
-	ans = requests.post('https://duckduckgo.com', data={'q':url_text,'format':'json', 'ia':'about'})
-	#print(ans.url)
-	#print(ans.text)
-	ans = ans.text
-	parsed = BeautifulSoup(ans)
-	#print("!!!",parsed)
-	topics = parsed.findAll('div', {'id':'zci-about'})#[0][0][0][1]
-#	topic = topic.findAll('div', {'class':'c-info--cw  cw has-aux'})
-#	topic = topic.findAll('div', {'class':'czi-main  c-info'})
-#	topic = topic.findAll('div', {'class':'czi-body'})
-#	topic = topic.findAll('div', {'class':'c-info__body'})
-	ans = topics.findAll('div', {'class': re.compile('results_*')})
-	ans = ans[1].text
-	print(topic)
-	print("\n\n", ans)
-	if len(ans) <= 5:
-		BadRequest()
+def GetQuest(request):
+	for i in range(len(requests.post('https://ru.wikipedia.org/w/api.php', data={'action':'opensearch', 'search':request, 'format':'json'}).json())):
+		ans = (requests.post('https://ru.wikipedia.org/w/api.php', data={'action':'opensearch', 'search':request, 'format':'json'}).json())[2][i]
+		if len(ans) > 21:
+			qi = i
+			break
+	bb = 0
+	eb = 0
+	todel = ""
+	for i in ans:
+		#print(i, bb, eb, todel)
+		if i == "(":
+			bb += 1
+		elif i == ")":
+			eb += 1
+		if bb > 0:
+			todel += i
+		if (bb == eb) and (bb > 0):
+			ans = ans.replace(todel, "")
+			todel = ""
+			bb = 0
+			eb = 0
+
+	print(ans)
+	if len(ans) < 5:
+		ans = BadRequest(ans)
+	else:
+		if qi > 0:
+			ans = "Возможно, вы имели ввиду "+ans
 	return ans
 
-def ChtoTakoe():
-	global l_text, arr_l_text, url_text
-	url_text = ""
-	mi = arr_l_text.index('такое') + 1
+
+def ChtoTakoe(text, text_arr, url):
+	url = ""
+	mi = text_arr.index('такое') + 1
 	mi1 = mi
-	while mi1 < len(arr_l_text):
+	while mi1 < len(text_arr):
 		if mi1 > mi:
-			url_text += " "
-		url_text += str(arr_l_text[mi1])
+			url += " "
+		url += str(text_arr[mi1])
 		mi1 += 1
-	ans = GetQuest()
+	ans = GetQuest(url)
 	return ans
 
 #def KtoTakoi(): #План "средниум"
@@ -56,7 +62,7 @@ arr_l_text = l_text.split()
 url_text = ""
 
 if l_text.find('что такое') != -1:
-	ans = ChtoTakoe()
+	ans = ChtoTakoe(l_text, arr_l_text, url_text)
 elif (l_text.find('кто такой') != -1) or (l_text.find('кто такая') != -1):
 	#План "средниум -- "KtoTakoi(l_text, arr_l_text)
 	ans = ChtoTakoe()

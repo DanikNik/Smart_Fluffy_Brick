b4b7e232fdcb251ffc91bc0647b0a75f74318aa3
slava <push_sla@mail.ru>
2016-07-23 12:35:10 +0300

    23.07, continue mock code

--- src1/quest.py
+++ src1/quest.py
@@ -2,9 +2,7 @@
 # -*- coding: utf-8 -*-
 import requests
 from bs4 import BeautifulSoup as beso
-
-#def NameBadRequest()#План "средниум"
-
+#################################################################################
 def GetThingQuest(request):
 	try:
 		for i in range(len(requests.post('https://ru.wikipedia.org/w/api.php', data={'action':'opensearch', 'search':request, 'format':'json'}).json())):
@@ -30,6 +28,29 @@ def GetThingQuest(request):
 				bb = 0
 				eb = 0
 
+		ans = ans.lower()
+##!Осторожно! Говнокод!!!
+		ans = ans.replace(" i ", " первый ")
+		ans = ans.replace(' ii ', ' второй ')
+		ans = ans.replace(' iii ', ' третий ')
+		ans = ans.replace(' iv ', ' четвёртый ')
+		ans = ans.replace(' v ', ' пятый ')
+		ans = ans.replace(' vi ', ' шестой ')
+		ans = ans.replace(' vii ', ' седьмой ')
+		ans = ans.replace(' viii ', ' восьмой ')
+		ans = ans.replace(' iix ', ' восьмой ')
+		ans = ans.replace(' ix ', ' девятый ')
+		ans = ans.replace(' x ', ' десятый ')
+		ans = ans.replace(' xi ', ' одиннадцатый ')
+		ans = ans.replace(' xii ', ' двенадцатый ')
+		ans = ans.replace(' xiii ', ' тринадцатый ')
+		ans = ans.replace(' xiv ', ' четырнадцатый ')
+		ans = ans.replace(' xv ', ' пятнадцатый ')
+		ans = ans.replace('(', '')
+		ans = ans.replace(')', '')
+		ans = ans.replace('англ.', '')
+		ans = ans.replace('фран.', '')
+		ans = ans.replace('франц.', '')
 		#print(ans)
 		if qi > 0:
 			ans = "Возможно, вы имели ввиду "+ans
@@ -37,7 +58,7 @@ def GetThingQuest(request):
 	except:
 		ans = "Я вас не понял, перефразируйте запрос"
 		return ans
-
+#################################################################################
 def WhatIs(text, text_arr):
 	url = ""
 	try:
@@ -46,7 +67,13 @@ def WhatIs(text, text_arr):
 		try:
 			mi = text_arr.index('такой') + 1
 		except:
-			mi = text_arr.index('такое') + 1
+			try:
+				mi = text_arr.index('такое') + 1
+			except:
+				try:
+					mi = text_arr.index('такие') + 1
+				except:
+					mi = text_arr.index('значит') + 1
 	mi1 = mi
 	while mi1 < len(text_arr):
 		if mi1 > mi:
@@ -54,34 +81,31 @@ def WhatIs(text, text_arr):
 		url += str(text_arr[mi1])
 		mi1 += 1
 	ans = GetThingQuest(url)
-	return ans.lower()
-
-def GetFreeQuest(request):
-	ans = requests.get('http://www.nigma.ru/?t=web&lang=all&srt=0&s='+request).text
-	soup = beso(ans)
-	print(ans)
-	ans = soup.findAll('li', {'value':'1'})
 	return ans
-	print(ans, ".")
-
-def FreeQuest(text, text_arr):
-	url = ""
-	for i in range(len(text_arr)):
-		if i != 0:
-			if i != len(text_arr) - 1:
-				url += text_arr[i]+" "
-			else:
-				url += text_arr[i]
-	print(url, ".")
-	ans = GetFreeQuest(url)
-
-
+#################################################################################
+def Trans(text, lang):
+	langs = {'русский':'ru','английский':'en','немецкий':'de','испанский':'es','французский':'fr','китайский':'zh','греческий':'el','иврит':'he','тайский':'th','эстонский':'et','японский':'ja','грузинский':'ka'}
+	try:
+		mi = text.find("переведи на ")
+		a = list(langs.keys()).index(lang)
+		mi = mi + len(list(langs.keys())[a]) + 12
+		text = text[mi:]
+		lang = langs.get(lang)
+		ans = requests.post('https://translate.yandex.net/api/v1.5/tr.json/translate', data={'key':'trnsl.1.1.20160723T075443Z.3071860cf4a85efc.3fef18c09d8253d502944581b4360833ed8749f2','lang':lang,'text':text}).json()
+		ans = ans['text'][0]
+	except:
+		ans = "Данный язык недоступен. Доступные языки: "+str(list(langs.keys()))
+	return ans
+#################################################################################
 text = input()##Способ общения: input или open
 l_text = text.lower()
 arr_l_text = l_text.split()
-
-if (l_text.find('кто такой') != -1) or (l_text.find('кто такая') != -1) or (l_text.find('кто такое') != -1) or (l_text.find('что такое') != -1):
-	ans = WhatIs(l_text, arr_l_text)
+#################################################################################
+if (arr_l_text[0] == "переведи") or (arr_l_text[1] == "переведи"):
+	lang = arr_l_text[2]
+	ans = Trans(l_text, lang)
+elif (l_text.find('кто такой') != -1) or (l_text.find('кто такая') != -1) or (l_text.find('кто такое') != -1) or (l_text.find('что такое') != -1) or (l_text.find('кто такие') != -1) or ((l_text.find('что значит')) != 1):
+			ans = WhatIs(l_text, arr_l_text)
 else:
-	ans = FreeQuest(l_text, arr_l_text)
+	ans = FreeQuest(arr_l_text, arr_l_text)
 print(ans)
